c7c6b78d70899dcd48e9947b9b11a03ed00f1b92
abc <abc@example.com>
2018-03-18 11:47:16 -0700

    beer

--- hungry.py
+++ hungry.py
@@ -3,4 +3,6 @@ if hungry=='yes':
     print("eat some food")
     print("eat some pizza")
 else:
-    print("do youyr hw")
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+         print("drink beer")
